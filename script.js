d4dd806d972e96774ee02fb74bf47f81050645af
Uncle-Ma <arkcitidal@gmail.com>
2026-02-21 12:13:02 -0500

    changed

---
+++ script.js
@@ -0,0 +1,61 @@
+fetch('components/nav.html')
+.then(response => response.text())
+.then(data => document.getElementById('nav-placeholder').innerHTML = data);
+
+fetch('components/footer.html')
+.then(response => response.text())
+.then(data => document.getElementById('footer-placeholder').innerHTML = data);
+
+  alert("This website is under construction and is not complete");
+
+
+
+
+const slides = document.querySelectorAll(".slides img");
+let slideIndex = 0;
+let intervalId = null;
+
+document.addEventListener("DOMContentLoaded", initializeSlider);
+
+function initializeSlider(){
+
+    if(slides.length > 0){
+    slides[slideIndex].classList.add("displaySlide");
+    intervalId = setInterval(nextSlide, 5000);
+
+    }
+
+}
+
+function showSlide(index) {
+
+
+    if(index >= slides.length){
+        slideIndex = 0;
+    }
+    else if (index < 0){
+        slideIndex = slides.length - 1;
+    }
+
+
+
+
+    slides.forEach(slide => {
+        slide.classList.remove("displaySlide");
+    });
+    slides[slideIndex].classList.add("displaySlide");
+
+}
+
+function prevSlide(){
+    clearInterval(intervalId);
+    slideIndex--;
+    showSlide(slideIndex);
+
+}
+
+function nextSlide(){
+    slideIndex++;
+    showSlide(slideIndex);
+}
+
